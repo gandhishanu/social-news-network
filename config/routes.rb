23547ab1c1cfdfa26e3b6ef97f1d0bd687ca1e40
Haider Hameed <haiderhameed@gmail.com>
2015-11-03 22:04:46 -0600

    navbar done, added menus

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   get '/logout', :to => 'sessions#destroy'
 
+  root :to => "posts#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
