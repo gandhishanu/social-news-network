60259fe78c2a0aba90892b2702c4d6b87ab1bd91
Haider Hameed <haiderhameed@gmail.com>
2015-11-07 16:05:27 -0600

    session refactoring

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,14 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-
     if session[:user_id]
       User.find(session[:user_id]).add_provider(auth_hash)
-      redirect_to root_path
     else
       auth = Authorization.find_or_create(auth_hash)
       session[:user_id] = auth.user.id
-      redirect_to root_path
     end
+    set_current_user
+    redirect_to root_path
   end
 
   def failure
