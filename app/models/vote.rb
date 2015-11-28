2e8b9fceb52379322e4ad01a8fbd59384becc33f
Haider Hameed <haider-hameed@uiowa.edu>
2015-11-28 11:21:24 -0600

    posts showing vote stats along with current user votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,7 @@
+require 'simple_enum'
 class Vote < ActiveRecord::Base
+  include SimpleEnum
   belongs_to :post
   belongs_to :user
-  as_enum :updown, [:upvote, :downvote], map: :string
+  as_enum :updown, upvote: 0, downvote: 1
 end
