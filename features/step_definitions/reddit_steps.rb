f89034f204d357dab1d7fa031cc1c5ba17622064
Haider Hameed <haider-hameed@uiowa.edu>
2015-12-10 00:14:56 -0600

    second scenerio completed for reddit algo bdd

--- features/step_definitions/reddit_steps.rb
+++ features/step_definitions/reddit_steps.rb
@@ -20,8 +20,24 @@ Then(/^I should see newest posts first$/) do
   expect(result).to be_truthy
 end
 
-Given(/^there are (\d+) posts created on same date but different number of upvotes$/) do |arg1|
+Given(/^there are (\d+) posts created on same date but different number of upvotes$/) do |post_count|
+  time = Time.parse('2015-12-09 00:00:00')
+  (1..post_count.to_i).each do |p|
+    Post.create!({id: p, title: "Post #{p}", body: "body", thumbnail: 'tn.jpg', category_id:1, created_at: time})
+  end
+  (1..post_count.to_i).each do |u|
+    User.create!({id: u, name: 'name', email: "email#{u}@email.com", password:'password'})
+  end
+  (1..post_count.to_i).each do |p|
+    (1..p).each do |u|
+      Vote.create!({updown_cd: 0, user_id: u, post_id: p})
+    end
+  end
 end
 
 Then(/^I should see posts with most up votes first$/) do
+  posts = (1..Post.count).map{|p| "Post #{p}"}.reverse
+  titles = posts.join(".*")
+  result = /#{titles}/m =~ page.body
+  expect(result).to be_truthy
 end
